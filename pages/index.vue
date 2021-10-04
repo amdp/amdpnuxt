1499d499e7f31cdb6ac2e5b2f7e1affabc734244
Alessandro Merletti De Palo <alessandromerlettidepalo@gmail.com>
2021-10-04 15:55:34 +0200

    update

--- pages/index.vue
+++ pages/index.vue
@@ -35,27 +35,45 @@
           Science.<br />
         </div>
       </b-row>
-      <b-row class="w-100 justify-content-center m-3">
-        <b-col cols="12" class="text-center">
+      <b-row>
+        <div class="go bb righteous">ME</div>
+        &nbsp;&nbsp;
+        <div class="base jj f">
+          I have been diagnosed in 2021 to be in the Autism spectrum, level 1
+          "<a href="https://en.wikipedia.org/wiki/Asperger_syndrome">Asperger</a
+          >", with no apparent disability and with high level of empathy. I am
+          in fact also an
+          <a href="https://hsperson.com/test/highly-sensitive-test">HSP-E</a>
+          person, an ex <a href="https://www.mensa.org/">Mensa</a> member and my
+          personality is
+          <a href="https://www.16personalities.com/">ENFJ-ENFP-A</a>,
+          <a href="https://www.jovianarchive.com/get_your_chart">
+            Human Design
+          </a>
+          ManGen 1/3-1/4,
+          <a href="https://www.truity.com/test/big-five-personality-test">
+            5 Big Personalities
+          </a>
+          Openness 84 Concentiousness 92 Extraversion 78 Agreableness 71
+          Neuroticism 38,
+          <a href="https://www.truity.com/test/enneagram-personality-test">
+            Enneagram
+          </a>
+          8-5,
+          <a href="https://www.5lovelanguages.com/quizzes/singles-quiz">
+            Love Language
+          </a>
+          Quality Time 30 Physical Touch 28 Acts of service 20 Words of
+          Affirmation 18 Receiving gifts 4.
+          <br /><br />
+          So many tests just to tell you I am human and happy to know new
+          people, so feel free to write me, you may find me in many social
+          networks.
+          <br />
           Hoping you found good informations, I wish you a pleasant and happy
-          day.
-        </b-col>
+          day.<br /><br /><br />
+        </div>
       </b-row>
     </b-row>
   </b-container>
 </template>
-
-<!--
-Traits:<br /><br />
-Asperger Sindrome - Autism level 1 diagnosed
-HSP: HSP-E <br />
-https://hsperson.com/test/highly-sensitive-test<br />
-16P: ENFJ-ENFP-A <br />
-https://www.16personalities.com<br />
-Human Design ManGen 1/3-1/4 <br />
-https://www.jovianarchive.com/get_your_chart<br />
-5BIGPERSONALITIES: I E 78   II ES 65   III A 71  IV C 92   V I 84
-https://www.truity.com/test/big-five-personality-test
-LOVELANGUAGE: Quality Time 30% Physical Touch 28% Acts of service 20% Words of Affirmation 18% Receiving gifts 4%
-https://www.5lovelanguages.com/quizzes/singles-quiz<br />
--->
