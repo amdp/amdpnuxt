e73d14b41c6c425a6e90e5369ee6368659fc4db8
Alessandro Merletti De Palo <alessandromerlettidepalo@gmail.com>
2022-01-06 18:26:20 +0100

    index poems edit

--- pages/poems/poems.vue
+++ pages/poems/poems.vue
@@ -4,19 +4,23 @@
       <h1>POEMS</h1>
     </b-row>
     <b-row class="m-3 justify-content-center">
-      <h2><nuxt-link to="./piccolaluce">Piccola luce</nuxt-link></h2>
+      <h2><nuxt-link to="./piccolaluce">Piccola luce 2021 12 15</nuxt-link></h2>
     </b-row>
     <b-row class="m-3 justify-content-center">
-      <h2><nuxt-link to="./noiosetto">Noiosetto</nuxt-link></h2>
+      <h2><nuxt-link to="./noiosetto">Noiosetto 2021 12 3</nuxt-link></h2>
     </b-row>
     <b-row class="m-3 justify-content-center">
-      <h2><nuxt-link to="./lefaccende">Le Faccende</nuxt-link></h2>
+      <h2><nuxt-link to="./lefaccende">Le Faccende 2021 12 20</nuxt-link></h2>
     </b-row>
     <b-row class="m-3 justify-content-center">
-      <h2><nuxt-link to="./misticanza">Misticanza</nuxt-link></h2>
+      <h2>
+        <nuxt-link to="./misticanza">
+          Misticanza 2021 12 21 Winter Solstice
+        </nuxt-link>
+      </h2>
     </b-row>
     <b-row class="m-3 justify-content-center">
-      <h2><nuxt-link to="./epifania">Epifania</nuxt-link></h2>
+      <h2><nuxt-link to="./epifania">Epifania 2022 1 6</nuxt-link></h2>
     </b-row>
     <b-row class="m-3 justify-content-center">
       <h5><nuxt-link to="../">- Back to main page -</nuxt-link></h5>
