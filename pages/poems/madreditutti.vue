56732dd02550bb3c382f7cb3e67b90c04f9893d0
Alessandro Merletti De Palo <alessandromerlettidepalo@gmail.com>
2022-03-02 13:06:58 +0100

    layout poem

--- pages/poems/madreditutti.vue
+++ pages/poems/madreditutti.vue
@@ -21,66 +21,69 @@
         <br />
         nema<br />
       </p>
-      <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
-      Padre nostro dall'aramaico per comparazioni: <br />
-      Avu-n<br />
-      Padre- noi<br />
-      <br />
-      d-b-ishmayya<br />
-      quello- nel- cielo<br />
-      <br />
-      net-qaddash<br />
-      [passivo]+rendi sacro<br />
-      <br />
-      shimmu-kh<br />
-      nome- te<br />
-      <br />
-      te-teh<br />
-      è ed è (è ed è, sia già)<br />
-      <br />
-      malciutu-kh<br />
-      regno- te<br />
-      <br />
-      neh-ve sovyanu-kh<br />
-      [passivo]-fatta volontà- te<br />
-      <br />
-      eiciana d-b-ismayya<br />
-      uguale quello-nei-cielo<br />
-      <br />
-      ap b-arah<br />
-      uguale nella terra<br />
-      <br />
-      hal-la-n<br />
-      Dai-a-noi<br />
-      <br />
-      lakhma <br />
-      pane<br />
-      <br />
-      d-sunqana-n<br />
-      quello-bisongo- noi<br />
-      <br />
-      yumana<br />
-      giorno<br />
-      <br />
-      u shvuq la-n<br />
-      e lascia a-noi<br />
-      <br />
-      khobei-n<br />
-      debiti-noi<br />
-      <br />
-      eiciana d-ap-akhna<br />
-      uguale quello-uguale-anche-noi<br />
-      <br />
-      shvuqqan l-khayavi-n<br />
-      lasciamo a-debitori-noi<br />
-      <br />
-      u-lla tella-n l-nessyuna<br />
-      e-no passare-noi a-prova<br />
-      <br />
-      ella passa-n men Bisha<br />
-      e-ma allontana-noi da Cattivo<br />
-      <br />
-      <br />
+      <p>
+        <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+        Padre nostro dall'aramaico per comparazioni: <br />
+        <br />
+        Avu-n<br />
+        Padre- noi<br />
+        <br />
+        d-b-ishmayya<br />
+        quello- nel- cielo<br />
+        <br />
+        net-qaddash<br />
+        [passivo]+rendi sacro<br />
+        <br />
+        shimmu-kh<br />
+        nome- te<br />
+        <br />
+        te-teh<br />
+        è ed è (è ed è, sia già)<br />
+        <br />
+        malciutu-kh<br />
+        regno- te<br />
+        <br />
+        neh-ve sovyanu-kh<br />
+        [passivo]-fatta volontà- te<br />
+        <br />
+        eiciana d-b-ismayya<br />
+        uguale quello-nei-cielo<br />
+        <br />
+        ap b-arah<br />
+        uguale nella terra<br />
+        <br />
+        hal-la-n<br />
+        Dai-a-noi<br />
+        <br />
+        lakhma <br />
+        pane<br />
+        <br />
+        d-sunqana-n<br />
+        quello-bisongo- noi<br />
+        <br />
+        yumana<br />
+        giorno<br />
+        <br />
+        u shvuq la-n<br />
+        e lascia a-noi<br />
+        <br />
+        khobei-n<br />
+        debiti-noi<br />
+        <br />
+        eiciana d-ap-akhna<br />
+        uguale quello-uguale-anche-noi<br />
+        <br />
+        shvuqqan l-khayavi-n<br />
+        lasciamo a-debitori-noi<br />
+        <br />
+        u-lla tella-n l-nessyuna<br />
+        e-no passare-noi a-prova<br />
+        <br />
+        ella passa-n men Bisha<br />
+        e-ma allontana-noi da Cattivo<br />
+        <br />
+        <br />
+      </p>
     </b-row>
     <b-row class="m-3 justify-content-center">
       <h5><nuxt-link to="./poems">- Back to poems list -</nuxt-link></h5>
